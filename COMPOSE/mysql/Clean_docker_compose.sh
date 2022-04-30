4379e0776132f2fc7316e1819b4614a73e06159b
Cheedge <cheedgelee@gmail.com>
2022-04-30 06:21:10 -0400

    clean log files and kill all containers and remove imgs

--- COMPOSE/mysql/Clean_docker_compose.sh
+++ COMPOSE/mysql/Clean_docker_compose.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# rm all log files of mysql, redis
 
 rm -rf ./master/datalog/*
 rm -rf ./slave/datalog/*
 rm -rf ../flask/project/uwsgi.log
 rm -rf ../redis/myredis/redis_6379.log
 
+# stop running containers
 docker ps | grep "_v1" | awk '{print $1}' | xargs docker stop
-
+# kill version v1 containers
 docker ps -a | grep "_v1" | awk '{print $1}' | xargs docker rm
-
+# remove images used in compose
 docker images | grep compose | awk '{print $3}' | xargs docker rmi
